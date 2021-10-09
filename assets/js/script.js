dbfe5dceb2e396cc35c8b1c1756f082f1a2b540b
Kate Hamilton <kate.hamilton828@gmail.com>
2021-10-09 13:08:10 +0300

    for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ const questionElement = document.getElementById ('questions');
 const finalScoreEl = document.getElementById('final-score');
 const initialsEl = document.getElementById('initials');
 let currentQuestionIndex= 0;
+var score= 0;
 
  
 
@@ -15,7 +16,7 @@ startButton.addEventListener('click', startGame);
 // Function to start the Game 
 function startGame () {
 currentQuestionIndex= 0;
-askQuestion();
+// askQuestion();
 
 // timer
 var seconds = document.getElementById("countdown").textContent;
@@ -38,11 +39,29 @@ var countdown = setInterval(function() {
 
 // Function to make questions appear
 
-function askQuestion() {
-    var askQuestions = allQuestions[currentQuestionIndex].question;
-    document.getElementById('questions').innerHTML = askQuestions;
+// function askQuestion() {
+//     var askQuestions = allQuestions[currentQuestionIndex].question;
+//     document.getElementById('questions').innerHTML = askQuestions;
 
-}
+// }
+
+for(var i=0; i < allQuestions.length; i++) {
+    var response= window.prompt(allQuestions[i].prompt);
+    if (response == questions[i].answer) {
+        score++;
+        alert('correct!');
+    } else {
+        alert('wrong!');
+    }
+ }  
+ alert ('you got' + score + "/" + questions.length)
+
+
+
+
+
+
+
 
 
 // to end the game 
@@ -59,23 +78,13 @@ function endGame () {
 
 var allQuestions = [
     {
-      question: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts",
+      prompt: "What color is a banana?\n(a) Red/Green\n(b) purple\n(c) yellow ",
+      answer: "c"
+    
     },
     {
-      question: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses",
-    },
-    {
-      question: "Arrays in Javascript can be used to store ____.",
-      choices: [
-        "numbers and strings",
-        "other arrays",
-        "booleans",
-        "all of the above",
-      ],
-      answer: "all of the above",
-    },
+        prompt: "What color is a banana?\n(a) Red/Green\n(b) purple\n(c) yellow ",
+        answer: "c"
+      
+      }
 ];
